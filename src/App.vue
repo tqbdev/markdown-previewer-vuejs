7a24e26d9880d530cf3d4eab149b926334c0076c
tqbdev <tranquocbao3897@gmail.com>
2018-10-04 22:13:49 +0700

    Upload project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col text-center">
+        <h1 class="light">Markdown Preview</h1>
+      </div>
+    </div>
+
+    <div class="row mt-4">
+      <div class="col-md-6">
+        <h4 class="light">Markdown</h4>
+        <textarea
+          class="info"
+          v-model="markdown">
+        </textarea>
+      </div>
+      <div class="col-md-6">
+        <h4 class="light">Preview</h4>
+        <div
+          :key="markdown"
+          class="info"
+          v-markdown
+          >{{markdown}}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data () {
+    return {
+      markdown: ''
+    };
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+textarea {
+  resize: none;
+  border: 2px dashed orange;
+  outline: none;
+}
+
+.info {
+  height: 400px;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.9);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import markdown from '@/directives/markdown';
+import './assets/style.css';
 
 Vue.config.productionTip = false
+
+Vue.directive('markdown', markdown);
 
 new Vue({
   render: h => h(App)
